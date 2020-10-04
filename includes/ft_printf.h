38bcb9fc7244febff22650aee78c967a57e3d1b2
clorin <cyrille.lorin@yahoo.fr>
2020-10-04 15:50:05 +0200

    creation

---
+++ includes/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clorin <clorin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/04 15:46:18 by clorin            #+#    #+#             */
+/*   Updated: 2020/10/04 15:49:15 by clorin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+int		ft_printf(const char *format, ...);
+
+#endif
