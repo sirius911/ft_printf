772a10359331a0fa2d9bcbb4584de38529145715
clorin <cyrille.lorin@yahoo.fr>
2020-10-12 11:25:20 +0200

    commun

--- src/ft_putx.c
+++ src/ft_putx.c
@@ -12,42 +12,6 @@
 
 #include "../includes/ft_printf.h"
 
-static int			hex_len(unsigned int nbr, char *base)
-{
-	int				i;
-	int				len_base;
-
-	i = 0;
-	if (nbr == 0)
-		return (1);
-	len_base = ft_strlen(base);
-	if (len_base == 0)
-		return (0);
-	while (nbr > 0)
-	{
-		i++;
-		nbr /= len_base;
-	}
-	return (i);
-}
-
-static int			ft_print_nbr_base(unsigned nbr, char *base)
-{
-	int				i;
-	int				len_base;
-
-	i = 0;
-	len_base = ft_strlen(base);
-	if (nbr >= len_base)
-	{
-		ft_print_nbr_base(nbr / len_base, base);
-		ft_print_nbr_base(nbr % len_base, base);
-	}
-	else
-		ft_putchar(base[nbr % len_base]);
-	return (hex_len(nbr, base));
-}
-
 int					ft_putx(t_flags *flags, va_list param, char *base)
 {
 	int				i;
@@ -58,7 +22,7 @@ int					ft_putx(t_flags *flags, va_list param, char *base)
 	i = 0;
 	len = 0;
 	nbr = (unsigned int)va_arg(param, unsigned int);
-	len_hex = hex_len(nbr, base);
+	len_hex = ft_len_nbr_base(nbr, base);
 	len = (flags->precision > len_hex) ? flags->precision : len_hex;
 	if (flags->precision == 0 && nbr == 0)
 		return (i += ft_putnchar(' ', flags->width));
