70f3d49d50b5ab7538cb88aa314793f1a99acb81
clorin <cyrille.lorin@yahoo.fr>
2020-10-12 11:51:44 +0200

    norme

--- src/ft_puts.c
+++ src/ft_puts.c
@@ -60,7 +60,7 @@ int			ft_puts(t_flags *flags, va_list param)
 	else
 	{
 		if (flags->zero)
-			i += ft_putnchar('0', flags->width - len);			
+			i += ft_putnchar('0', flags->width - len);
 		else
 			i += ft_putnchar(' ', flags->width - len);
 		i += ft_print_s(str, flags, len);
